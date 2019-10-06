4cb4ee7b459b2851966eb7ff998736a8564c220f
Justin Phillips <justinphil02@yahoo.com>
2019-10-06 02:45:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,12 +3,13 @@ def run_guessing_game
   number = rand(6) + 1 
   puts "Guess a number from 1 - 6"
   guess = gets
+  
   if number == guess
-    puts "You guessed the correct number!"
+  puts "You guessed the correct number!"
   elsif guess == "exit"
   puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{number}."
+  puts "Sorry! The computer guessed #{number}."
   end
 
 end
